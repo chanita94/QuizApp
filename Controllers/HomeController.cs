0f56e361e3420a53d8a99737e4fec0008873a067
chanita <chanitapav@gmail.com>
2025-10-29 22:10:34 +0200

    adding some css

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,14 +5,14 @@ using QuizApp.Models;
 
 namespace QuizApp.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller//handles requests for the home page.
     {
 
-        public IActionResult Index()
+        public IActionResult Index() //default action method 
         {
-            var json = System.IO.File.ReadAllText("Data/quizzes.json");
-            var quizzes = JsonSerializer.Deserialize<List<Quiz>>(json);
-            return View(quizzes);
+            var json = System.IO.File.ReadAllText("Data/quizzes.json");//Reads JSON
+            var quizzes = JsonSerializer.Deserialize<List<Quiz>>(json);//convert JSON into a list
+            return View(quizzes); //Pass quizzes to the page.
         }
 
     }
--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -5,44 +5,44 @@ using System.Text.Json;
 
 namespace QuizApp.Controllers
 {
-    public class QuizController : Controller
+    public class QuizController : Controller//handles actions for displaying and submitting quizzes.
     {
-        private List <Quiz> LoadQUizzes()
+        private List<Quiz> LoadQUizzes()//method for loading quizzes from json
         {
-            var json = System.IO.File.ReadAllText("Data/quizzes.json");
-            return JsonSerializer.Deserialize<List<Quiz>>(json);
+            var json = System.IO.File.ReadAllText("Data/quizzes.json");//Reads JSON
+            return JsonSerializer.Deserialize<List<Quiz>>(json);//convert JSON into a list
         }
-        public IActionResult Index(int id)
+        public IActionResult Index(int id)//Displays a specific quiz
         {
-            var quiz = LoadQUizzes().FirstOrDefault(x => x.Id == id);
-            if (quiz == null)
+            var quiz = LoadQUizzes().FirstOrDefault(x => x.Id == id);//Find the quiz with that id
+            if (quiz == null)//check is this quiz exist
             {
                 return NotFound();
             }
-            return View("Take", quiz);
+            return View("Take", quiz);//returns take view with the specific quiz
         }
         [HttpPost]
-        public IActionResult Submit(int id, List<int>answers)
+        public IActionResult Submit(int id, List<int> answers)//method for submiting when user completes quiz.
         {
-            var quiz = LoadQUizzes().FirstOrDefault(X => X.Id == id);
-            if (quiz == null)
+            var quiz = LoadQUizzes().FirstOrDefault(X => X.Id == id);//Find the quiz with that id
+            if (quiz == null)//check is this quiz exist
             {
                 return NotFound();
             }
-            int score = 0;
-            for (int i = 0; i < quiz.Questions.Count; i++)
+            int score = 0;//
+            for (int i = 0; i < quiz.Questions.Count; i++)//Loop through each question 
             {
-                Console.WriteLine(quiz.Questions[i].CorrectAnswer);
-                if (i<answers.Count && answers[i] == quiz.Questions[i].CorrectAnswer)
+                if (i < answers.Count && answers[i] == quiz.Questions[i].CorrectAnswer)//compare the answer to the correct one
                 {
                     score++;
                 }
             }
+            //Storing data in ViewBag to use it in  the Result view
             ViewBag.Score = score;
             ViewBag.Total = quiz.Questions.Count;
-            ViewBag.Quiz = quiz;           // send the quiz object
-            ViewBag.Answers = answers;     // send submitted answers
-            return View("Result");
+            ViewBag.Quiz = quiz;          
+            ViewBag.Answers = answers;
+            return View("Result");//Loads Result view 
         }
     }
 }
